ca7fb4a6c52807e074637e09ea0064d73532f42a
Alex Bates <16batesa@gmail.com>
2015-02-08 19:07:01 +0000

    Update settings.js

--- code/settings.js
+++ code/settings.js
@@ -1,15 +1,16 @@
 // this code is run if the user has created the project
 if(!scratchext.isShared) {
-  // project is unshared
+  // replace share button
   scratchext.log('replacing share button');
   scratchext.addCSS(scratchext.root + 'css/share-btn.css');
-  
   $('html').append('<div id="share-btn" class="editorOnly">Share</div>');
   $('#share-btn').on('click', JSshareProject);
-  
   $('#share').on('click', JSshareProject);
   
+  // install notification thing
+  scratchext.addCSS(scratchext.root + 'css/scratchext-installed.css');
   $('.public').text('ScratchExt 2.0 is installed!');
+  // TODO: in-editor scratchext 2.0 is installed
 }
 
 $('#owner').text('you');
