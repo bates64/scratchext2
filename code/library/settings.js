cb214572f50b3bfbe04b6b1a90adbf9eb582f477
Alex Bates <16batesa@gmail.com>
2015-02-08 17:46:52 +0000

    Update settings.js

--- code/library/settings.js
+++ code/library/settings.js
@@ -1,1 +1,8 @@
+// this code is run if the user has created the project
 scratchext.log('todo settings', 'red');
+
+if(!scratchext.isShared) {
+  // project is unshared
+  scratchext.log('Project is unshared, replacing share button');
+  scratchext.addCSS(scratchext.root + 'css/share-btn.css');
+}
