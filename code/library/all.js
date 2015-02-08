b4a959d803b693edeeb83563695f6727f982e80a
Alex Bates <16batesa@gmail.com>
2015-02-08 17:23:29 +0000

    Update all.js

--- code/library/all.js
+++ code/library/all.js
@@ -3,3 +3,4 @@ $.getScript(scratchext.root+'/library/keys.js');
 $.getScript(scratchext.root+'/library/project.js');
 $.getScript(scratchext.root+'/library/speech.js');
 $.getScript(scratchext.root+'/library/web.js');
+$.getScript(scratchext.root+'/library/ripple.js');
