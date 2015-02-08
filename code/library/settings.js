f888e22750a6b030abb3e79ae4fb8f738a70fc82
Alex Bates <16batesa@gmail.com>
2015-02-08 18:06:09 +0000

    Update settings.js

--- code/library/settings.js
+++ code/library/settings.js
@@ -6,5 +6,5 @@ if(!scratchext.isShared) {
   scratchext.log('Project is unshared, replacing share button');
   scratchext.addCSS(scratchext.root + 'css/share-btn.css');
   
-  var things = $('body').append('<div id="share-btn" class="editorOnly">Share</div>');
+  $('html').append('<div id="share-btn" class="editorOnly">Share</div>');
 }
