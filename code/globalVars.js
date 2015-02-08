1d0ae2e1452cc4e5e06033fdc8882a3e9d2af629
Alex Bates <16batesa@gmail.com>
2015-02-08 17:43:56 +0000

    Update globalVars.js

--- code/globalVars.js
+++ code/globalVars.js
@@ -4,6 +4,7 @@ var scratchext = {
     creator: Scratch.INIT_DATA.PROJECT.model.creator,
     author: Scratch.INIT_DATA.LOGGED_IN_USER.model.username === Scratch.INIT_DATA.PROJECT.model.creator,
     installed: [],
+    isShared: Scratch.FlashApp.model.attributes.isPublished,
     id: Scratch.INIT_DATA.PROJECT.model.id,
     root: 'https://rawgit.com/GrannyCookies/scratchext2/master/code/'
 };
