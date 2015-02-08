fb8f61e45a52a945d41f1952bcfbb049294480be
Alex Bates <16batesa@gmail.com>
2015-02-08 09:41:52 +0000

    Update start.js

--- code/start.js
+++ code/start.js
@@ -34,13 +34,18 @@ function begin() {
     addCSS('http://www.stefanbates.com/scratchext2/code/icon.css');
     addJS('http://www.stefanbates.com/scratchext2/code/sweet-alert.min.js');
 
-    setTimeout(function() {
-        scratchext.log('GITHUB VER LOADED', 'green');
+    function go() {
+        if(scratchext===undefined) {
+            setTimeout(go, 500);
+            return;
+        }
+        scratchext.log('Loaded ScratchExt 2.0', 'green');
         
         // install buttons
         $('.stats').first().append('<div class="action tooltip bottom installscratchext"><span class="scratchexticon icon">ScratchExt</span></div>');
         $('body').append('<div class="installscratchext" id="editorInstall"></div>');
 
+        // show/hide scratchext editor button if in editor or not
         setInterval(function() {
             if(scratchext.editMode()) {
                 $('#editorInstall').show();
@@ -70,53 +75,9 @@ function begin() {
                 });
             }
         });
-    }, 500);
-
-    // variables and functions for use with extensions
-    window.scratchext = {
-        username: Scratch.INIT_DATA.LOGGED_IN_USER.model.username,
-        creator: Scratch.INIT_DATA.PROJECT.model.creator,
-        installed: [],
-        id: Scratch.INIT_DATA.PROJECT.model.id,
-        root: 'https://rawgit.com/GrannyCookies/scratchext2/master/code/'
-    };
-
-    scratchext.projectJSON = function(id, callback) {
-        $.get("http://projects.scratch.mit.edu/internalapi/project/"+id.toString()+"/get/", function(data) {
-            callback(data);
-        });
     }
-
-    scratchext.editMode = function() {
-        return Scratch.FlashApp.isEditMode;
-    };
-
-    scratchext.notes = function() {
-        var notes;
-        if(scratchext.username === scratchext.creator) {
-            // the current user made this project
-            notes = $('textarea[name=description]').text().replace(/\s/g, "").toUpperCase();
-        } else {
-            // project was not shared by the current user
-            notes = $('.overview:eq(1)').text().replace(/\s/g, "").toUpperCase();
-        }
-
-        return notes;
-    };
-
-    scratchext.install = function(name, descriptor, extension) {
-        if(scratchext.installed.indexOf(name)===-1) {
-            scratchext.installed.push(name);
-            ScratchExtensions.register(name, descriptor, extension);
-        }
-    };
-
-    scratchext.log = function(string, color) {
-        if(color===undefined)
-            color = 'white';
-        
-        console.log('%c[%cScratchExt%c] %c'+string, 'color: white;', 'color: #986fe6;', 'color: white;', 'color: '+color+';');
-    }
+    
+    setTimeout(go, 500);
 }
 
 begin();
