d557dd8e9fd62a2ab121b42d6b7237448fdb89e7
Alex Bates <16batesa@gmail.com>
2015-02-11 19:07:25 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+// this should be our rawgit mini :D
 
 header('Content-Type: text/javascript');
+header("Access-Control-Allow-Origin: *");
+
 $url = filter_input(INPUT_GET,"link",FILTER_SANITIZE_STRING);
 echo $url;
-
 ?>
