e00e36bf224316cd3c0d671cd6dd20b9d700b554
Alex Bates <16batesa@gmail.com>
2015-02-11 18:18:39 +0000

    Update settings-btn.js

--- code/settings-btn.js
+++ code/settings-btn.js
@@ -18,13 +18,13 @@ $('#scratchext-settings').on('click', function() {
   $('#scratchext-settings-wrapper').append('<h1>ScratchExt 2.0 Settings</h1>');
   
   $('#scratchext-settings-close').on('click', function() {
-    $('#scratchext-settings-overlay').fadeOut();
-    $('#scratchext-settings-pane').fadeOut();
+    $('#scratchext-settings-overlay').fadeOut(300);
+    $('#scratchext-settings-pane').fadeOut(300);
     
     setTimeout(function() {
       scratchext.settingsOpen = false;
       $('#scratchext-settings-overlay').remove();
       $('#scratchext-settings-pane').remove();
-    }, 500);
+    }, 300);
   });
 });
