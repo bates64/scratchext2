609214ef1ffbfe4b945aa2b60587287f47e7f7f0
Alex Bates <16batesa@gmail.com>
2015-02-11 19:18:19 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-// this should be our rawgit mini :D
+// our rawgit mini
+// usage:
+// http://www.stefanbates.com/scratchext2/temp/index.php?p=start.js
 
 header('Content-Type: text/javascript');
 header("Access-Control-Allow-Origin: *");
 
-$url = $_GET['link'];
+$url = $_GET['p'];
 echo '<h1>ScratchExt 2.0 Rawgit Mini - Test</h1>';
 echo $url;
 ?>
