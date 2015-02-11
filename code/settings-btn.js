1a295c30885dbdcb36e27b5f00e7487d7749bd4c
Alex Bates <16batesa@gmail.com>
2015-02-11 17:49:07 +0000

    Update settings-btn.js

--- code/settings-btn.js
+++ code/settings-btn.js
@@ -1,10 +1,17 @@
 // add a button to the account menu
-$('.logout').before('<li><a id="scratchext-settings" href="javascript:void;">ScratchExt Settings</a></li>');
+$('.logout').before('<li><a id="scratchext-settings" href="javascript:void">ScratchExt Settings</a></li>');
 
 // when said button is clicked load some swag
 $('#scratchext-settings').on('click', function() {
+  if(scratchext.settingsOpen) {
+    return false;
+  }
+  
+  scratchext.settingsOpen = true;
   scratchext.log('Settings opened');
   
   addCSS(scratchext.root+'/css/settings.css');
-  $('body').append('<div id="scratchext-settings-pane"></div>').fadeIn;
+  $('body').append('<div id="scratchext-settings-pane"></div>').fadeIn();
+  
+  $('#scratchext-settings-pane').append('<h1>ScratchExt 2.0 Settings');
 });
