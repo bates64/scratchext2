e9ead19ac7ea178f9afd0bbf95d1b3e710a78a33
Alex Bates <16batesa@gmail.com>
2015-02-11 19:45:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@ header('Content-Type: text/javascript');
 header("Access-Control-Allow-Origin: *");
 
 $url = $_GET['p'];
-echo file_get_contents('https://raw.githubusercontent.com/GrannyCookies/scratchext2/master/code/' . $url);
+//echo file_get_contents('https://raw.githubusercontent.com/GrannyCookies/scratchext2/master/code/' . $url);
+echo file_get_contents('https://raw.githubusercontent.com/GrannyCookies/scratchext2/master/code/start.js');
 ?>
