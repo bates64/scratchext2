942141a8825c44d309a3dd670ff0180cb5a452fb
Alex Bates <16batesa@gmail.com>
2015-02-11 19:21:59 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,5 @@ header('Content-Type: text/javascript');
 header("Access-Control-Allow-Origin: *");
 
 $url = $_GET['p'];
-echo '<h1>ScratchExt 2.0 Rawgit Mini - Test</h1>';
-echo $url;
+echo file_get_contents('https://raw.githubusercontent.com/GrannyCookies/scratchext2/master/code/' . $url);
 ?>
