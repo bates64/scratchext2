403d74ebaf8bd4a0e67a089c9d78992aa7fe39e5
GrannyCookies <16batesa@gmail.com>
2015-02-11 19:05:08 +0000

    rawgit mini test

--- index.php
+++ index.php
@@ -1,36 +1,7 @@
 <?php
- if ($handle = opendir('.')) {
-   while (false !== ($file = readdir($handle)))
-      {
-          if ($file != "." && $file != ".." && $file != "index.php")
-	  {
-          	$thelist .= '<a href="'.$file.'">'.$file.'</a><br>';
-          }
-       }
-  closedir($handle);
-  }
+
+header('Content-Type: text/javascript');
+$url = filter_input(INPUT_GET,"link",FILTER_SANITIZE_STRING);
+echo $url;
+
 ?>
-<style>
-
-html {
-    font-family: sans-serif;
-    background-color: black;
-    color: white;
-}
-
-a {
-    text-decoration: none;
-    color: #4CCF2A;
-}
-
-a:hover {
-    color: #369A1C;
-}
-
-p {
-    line-height: 28px;
-}
-
-</style>
-<h1>ScratchExt 2.0 Source Code</h1>
-<p><?=$thelist?></p>
