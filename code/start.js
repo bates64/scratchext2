762d46d155b367a926f513292f60a157be563bd7
Alex Bates <16batesa@gmail.com>
2015-02-11 20:18:28 +0000

    Update start.js

--- code/start.js
+++ code/start.js
@@ -1,6 +1,6 @@
 if(localStorage['scratchext2ver'] !== 1) {
   alert('ScratchExt 2.0 needs updating!');
-  window.open('http://grannycookies.github.io/scratchext2/help/scratchext.user.js');
+  window.open('http://grannycookies.github.io/scratchext2/help/scratchext.user.js?i='+Date.now());
 } else {
   $.getScript('http://scratchextproxy.x10.mx/?p='+'load-btn.js');
 }
