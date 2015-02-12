da1afc5ff64d159598c816779927643a83fa23aa
Alex Bates <16batesa@gmail.com>
2015-02-12 17:02:21 +0000

    Update index.php

--- proxy/index.php
+++ proxy/index.php
@@ -2,14 +2,13 @@
 //http://scratchextproxy.x10.mx/?p=start.js
 header("Access-Control-Allow-Origin: *");
 
+echo '<h1>IN DEVELOPMENT</h1>';
 $url = 'https://raw.githubusercontent.com/GrannyCookies/scratchext2/master/code/' . $_GET['p'];
 
 // mime content detector
 $finfo = finfo_open($url);
 $mime_type = finfo_file($finfo, $filename);
 finfo_close($finfo);
-
-echo '<h1>IN DEVELOPMENT</h1>';
 
 // set MIME type based on extension
 echo $mime_type;
