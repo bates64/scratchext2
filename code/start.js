4676028349a40b4e94de4d8197364dc2e0ce1d62
Felizolinha <matheusbsb2009@Hotmail.com>
2015-02-12 17:22:25 -0200

    Removed unnecessary MIME type

--- code/start.js
+++ code/start.js
@@ -2,5 +2,5 @@ if(localStorage['scratchext2ver'] !== "1") {
   alert('ScratchExt 2.0 needs updating!');
   window.open('http://grannycookies.github.io/scratchext2/help/scratchext.user.js?i='+Date.now());
 } else {
-  $.getScript('http://scratchextproxy.x10.mx/?p='+'load-btn.js&c='+Date.now()+'&mime=text/javascript');
+  $.getScript('http://scratchextproxy.x10.mx/?p='+'load-btn.js&c='+Date.now());
 }
