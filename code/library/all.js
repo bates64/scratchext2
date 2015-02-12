b2b26ea5e6b191a8137a49a185c543dba9dc6641
Alex Bates <16batesa@gmail.com>
2015-02-12 17:40:43 +0000

    Delete all.js

--- code/library/all.js
+++
@@ -1,6 +0,0 @@
-// all library loading
-$.getScript(scratchext.root+'/library/keys.js');
-$.getScript(scratchext.root+'/library/project.js');
-$.getScript(scratchext.root+'/library/speech.js');
-$.getScript(scratchext.root+'/library/web.js');
-$.getScript(scratchext.root+'/library/ripple.js');
