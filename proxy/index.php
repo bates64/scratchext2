02273e23db13035b26d445ca5586d9d0b720b1f2
Alex Bates <16batesa@gmail.com>
2015-02-12 17:43:27 +0000

    Update index.php

--- proxy/index.php
+++ proxy/index.php
@@ -2,10 +2,16 @@
 //http://scratchextproxy.x10.mx/?p=start.js&mime=text/javascript
 header("Access-Control-Allow-Origin: *");
 
-$url = 'https://raw.githubusercontent.com/GrannyCookies/scratchext2/master/code/' . $_GET['p'];
 $mime_type = $_GET['mime'];
-
 header('Content-Type: ' . $mime_type);
+
+// default case
+$url = 'https://raw.githubusercontent.com/GrannyCookies/scratchext2/master/code/' . $_GET['p'];
+
+// special cases
+if ($url == 'datanarrative') {
+  $url = 'https://raw.githubusercontent.com/MrSherlockHolmes/NarrativeC/master/DataNarrative.js';
+}
 
 echo file_get_contents($url);
 ?>
