dfaafa65026ae92ab4f33e012f5fd8f68fb79f2f
Alex Bates <16batesa@gmail.com>
2015-02-12 16:45:21 +0000

    Update index.php

--- proxy/index.php
+++ proxy/index.php
@@ -1,9 +1,14 @@
 <?php
 //http://scratchextproxy.x10.mx/?p=start.js
 
-header('Content-Type: text/javascript');
 header("Access-Control-Allow-Origin: *");
 
 $url = $_GET['p'];
+
+// set MIME type based on extension
+$parts=explode(".", $_POST[$url]);
+echo $parts[count($parts)-1];
+header('Content-Type: text/javascript');
+
 echo file_get_contents('https://raw.githubusercontent.com/GrannyCookies/scratchext2/master/code/' . $url);
 ?>
