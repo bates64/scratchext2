83ba41e0fb60f9d812da86a5efb08159dfad2b89
Alex Bates <16batesa@gmail.com>
2015-02-13 19:05:13 +0000

    Update settings-btn.js

--- code/settings-btn.js
+++ code/settings-btn.js
@@ -21,8 +21,8 @@ $('#scratchext-settings').on('click', function() {
   scratchext.settings.all = [];
   
   // actual settings
-  scratchext.settings.add('editor-button', 'Should the editor button show?');
-  scratchext.settings.add('name', 'desc');
+  scratchext.settings.add('editor-button', 'In-editor "install scratchext" button');
+  scratchext.settings.add('1.0-button', '"install scratchext 1.0" button');
   scratchext.settings.load();
 
   // save button
