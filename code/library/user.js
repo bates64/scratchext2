056bc46b32e4adb30fbe5569e21b086841f84989
Alex Bates <16batesa@gmail.com>
2015-02-14 13:13:25 +0000

    Update user.js

--- code/library/user.js
+++ code/library/user.js
@@ -19,10 +19,20 @@ function installExtension() {
             url: scratchext.getWiki('user')
         };
 
+        // get userid
         ext.userid = function(user, callback) {
-          $.get("http://www.stefanbates.com/api/get_userid.php#"+user, function(html) {
-          	callback(html);
-          });
+            var img;
+            var data = window.location.href;
+            var username = data.substring(data.match('#').index+1, data.length);
+            $.get("http://scratch.mit.edu/users/"+username+'/', function(html) {
+            	data = html;
+            	var avatar = data.substring(data.match('<div class="avatar">').index, data.match('" width="55" height="55"').index)+'" id="avatarimagetemp">';
+            	$("html").append("<div id='#avatartemp' style='display:none;'>"+avatar+"</div>");
+                var src = $("#avatarimagetemp").attr("src");
+                var userid = src.substr(38, 7);
+                
+                callback(userid);
+            });
         };
 
         scratchext.install('User', descriptor, ext);
