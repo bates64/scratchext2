175bb2d6cec668989f13781225e88a9219bbfbc9
Alex Bates <16batesa@gmail.com>
2015-02-14 13:26:46 +0000

    Create default.js

---
+++ code/library/default.js
@@ -0,0 +1,44 @@
+// use this when making new extensions
+var name = 'Default';
+scratchext.libraries.name = {
+    name: name,
+    advanced: false
+};
+function installExtension() {
+    ScratchExtensions.unregister(name);
+    
+    (function(ext) {
+
+        ext._getStatus = function() {
+            return {
+                status: 2,
+                msg: 'Installed'
+            };
+        };
+
+        var descriptor = {
+            blocks: [
+            ],
+            
+            menus: {
+            },
+            
+            url: scratchext.getWiki(name.toLowerCase)
+        };
+        
+        if(scratchext.libraries.name.advanced) {
+            descriptor.blocks.push([' ', 'advanced is on!', 'nothing']);
+        } else {
+            descriptor.blocks.push(['!', 'advanced (test)', 'advanced']);
+        }
+        
+        // turn advanced on and reload extension
+        ext.advanced = function() {
+            scratchext.libraries.name.advanced = true;
+            installExtension();
+        }
+
+        scratchext.install(name, descriptor, ext);
+    })({});
+}
+installExtension();
