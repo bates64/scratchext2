9521492c2fc0df2b907039d04d89e832e92dd8b9
Alex Bates <16batesa@gmail.com>
2015-02-14 13:36:11 +0000

    Update settings-btn.js

--- code/settings-btn.js
+++ code/settings-btn.js
@@ -1,48 +1,15 @@
 scratchext.settings = {};
 
+function loadBtns() {
+  scratchext.settings.all = [];
+  scratchext.settings.add('editor-button', 'In-editor "install scratchext" button');
+  scratchext.settings.add('1-0-button', '"install scratchext 1.0" button & auto-install when detected');
+  scratchext.settings.add('share-button-overlay', 'Overlay share button in editor to allow you to share freely');
+  scratchext.settings.add('install-notify', 'Display a popup when ScratchExt 2.0 is installed');
+  scratchext.settings.load();
+}
+
 loadBtns();
-
-// add a button to the account menu
-$('.logout').before('<li><a id="scratchext-settings" href="javascript:void(0)">ScratchExt Settings</a></li>');
-
-// when said button is clicked load some swag
-$('#scratchext-settings').on('click', function() {
-  if(scratchext.settingsOpen) {
-    return false;
-  }
-
-  scratchext.settingsOpen = true;
-  scratchext.log('Settings: opened');
-  
-  addCSS(scratchext.css_root+'/css/settings.css');
-  $('body').append('<div id="scratchext-settings-overlay"></div>').fadeIn(300);
-  $('body').append('<div id="scratchext-settings-pane"><div id="scratchext-settings-wrapper"></div></div>');
-  
-  $('#scratchext-settings-wrapper').append('<a href="javascript:void(0)" id="scratchext-settings-close">x</a>');
-  $('#scratchext-settings-wrapper').append('<h1>ScratchExt 2.0 Settings</h1>');
-
-  loadBtns();
-
-  // save button
-  $('#scratchext-settings-wrapper').append('<br><h3><a href="javascript:scratchext.settings.save();" id="scratchext-settings-save">Save</a></h3>')
-
-  $('#scratchext-settings-close').on('click', function() {
-    if(scratchext.settings.currentData() !== scratchext.settings.savedData()) {
-      swal({
-        title: "Are you sure?",
-        text: "You have unsaved settings.",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonText: "Discard changes",
-        cancelButtonText: "Cancel",
-      }, function() {
-        scratchext.settings.close();
-      });
-    } else {
-      scratchext.settings.close();
-    }
-  });
-});
 
 scratchext.settings.add = function(name, desc) {
   scratchext.settings.all.push(name);
@@ -139,14 +106,47 @@ if(scratchext.settings.savedData()===undefined) {
   localStorage['scratchext-settings'] = JSON.stringify(to);
 }
 
+// add a button to the account menu
+$('.logout').before('<li><a id="scratchext-settings" href="javascript:void(0)">ScratchExt Settings</a></li>');
+
+// when said button is clicked load some swag
+$('#scratchext-settings').on('click', function() {
+  if(scratchext.settingsOpen) {
+    return false;
+  }
+
+  scratchext.settingsOpen = true;
+  scratchext.log('Settings: opened');
+  
+  addCSS(scratchext.css_root+'/css/settings.css');
+  $('body').append('<div id="scratchext-settings-overlay"></div>').fadeIn(300);
+  $('body').append('<div id="scratchext-settings-pane"><div id="scratchext-settings-wrapper"></div></div>');
+  
+  $('#scratchext-settings-wrapper').append('<a href="javascript:void(0)" id="scratchext-settings-close">x</a>');
+  $('#scratchext-settings-wrapper').append('<h1>ScratchExt 2.0 Settings</h1>');
+
+  loadBtns();
+
+  // save button
+  $('#scratchext-settings-wrapper').append('<br><h3><a href="javascript:scratchext.settings.save();" id="scratchext-settings-save">Save</a></h3>')
+
+  $('#scratchext-settings-close').on('click', function() {
+    if(scratchext.settings.currentData() !== scratchext.settings.savedData()) {
+      swal({
+        title: "Are you sure?",
+        text: "You have unsaved settings.",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Discard changes",
+        cancelButtonText: "Cancel",
+      }, function() {
+        scratchext.settings.close();
+      });
+    } else {
+      scratchext.settings.close();
+    }
+  });
+});
+
 // keep this here
 add_scratchext_buttons();
-
-function loadBtns() {
-  scratchext.settings.all = [];
-  scratchext.settings.add('editor-button', 'In-editor "install scratchext" button');
-  scratchext.settings.add('1-0-button', '"install scratchext 1.0" button & auto-install when detected');
-  scratchext.settings.add('share-button-overlay', 'Overlay share button in editor to allow you to share freely');
-  scratchext.settings.add('install-notify', 'Display a popup when ScratchExt 2.0 is installed');
-  scratchext.settings.load();
-}
