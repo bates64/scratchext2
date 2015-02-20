a6a69b36a6e3f7da4f7cfc222a06579b1464f34d
Alex Bates <16batesa@gmail.com>
2015-02-20 13:27:39 +0000

    Update user.js

--- code/library/user.js
+++ code/library/user.js
@@ -25,21 +25,8 @@ function installExtension() {
 
         // get userid
         ext.userid = function(user, callback) {
-            /*var img;
-            var data = window.location.href;
-            var username = data.substring(data.match('#').index+1, data.length);
-            $.get("http://scratch.mit.edu/users/"+username+'/', function(html) {
-            	data = html;
-            	var avatar = data.substring(data.match('<div class="avatar">').index, data.match('" width="55" height="55"').index)+'" id="avatarimagetemp">';
-            	$("html").append("<div id='#avatartemp' style='display:none;'>"+avatar+"</div>");
-                var src = $("#avatarimagetemp").attr("src");
-                var userid = src.substr(38, 7);
-                
-                callback(userid);
-            });*/
-            $.get("http://scratch.mit.edu/site-api/users/all/" + user + "/", function(raw) {
-                var json = JSON.parse(raw);
-                scratchext.log(json);
+            $.get("http://scratch.mit.edu/site-api/users/all/" + user + "/", function(data) {
+                callback(data['user']['pk']);
             });
         };
 
