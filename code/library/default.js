1a9182d1f4b1e303d8ed7a2f17e6563e33a4c656
Felizolinha <matheusbsb2009@Hotmail.com>
2015-02-21 18:32:05 -0200

    Added missing _shutdown function

--- code/library/default.js
+++ code/library/default.js
@@ -9,6 +9,8 @@ function installExtension() {
     
     (function(ext) {
 
+        ext._shutdown = function() {};
+
         ext._getStatus = function() {
             return {
                 status: 2,
