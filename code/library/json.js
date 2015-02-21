b075eb16ad33dc305ac86ff51dbb7376b6d75905
Felizolinha <matheusbsb2009@Hotmail.com>
2015-02-21 18:32:30 -0200

    Added missing _shutdown function

--- code/library/json.js
+++ code/library/json.js
@@ -4,6 +4,8 @@
 function installExtensionObject() {
     (function(ext) {
         
+        ext._shutdown = function() {};
+        
         ext._getStatus = function() {return {status: 2, msg: 'Ready'};};
         var descriptor = {
             blocks: [
